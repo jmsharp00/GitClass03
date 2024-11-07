76e0311b3a8ca62b113758d1e44f0e2bb8085d06
jmsharp00 <jessica.m.sharp00@gmail.com>
2024-11-07 15:03:03 +0900

    second commit

--- src/main/java/PracticeSend1.java
+++ src/main/java/PracticeSend1.java
@@ -1,5 +1,6 @@
 public class PracticeSend1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Trump won the USA election in 2024");
     }
 }
